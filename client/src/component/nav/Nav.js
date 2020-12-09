547f45e0a46caac3ee5cb781915c4eaa03f6fcdd
NemoTaek <vkfkdchfhr@gmail.com>
2020-12-09 15:34:04 +0900

    merge completed

--- client/src/component/nav/Nav.js
+++ client/src/component/nav/Nav.js
@@ -1,14 +1,12 @@
-import React, { useRef } from "react";
+import React, { useCallback, useRef } from "react";
 import { NavLink } from "react-router-dom";
+import { login, logout, rememberid, forgotid } from "../../modules/login"
+import { useSelector, useDispatch } from 'react-redux';
 import "./Nav.css"
 import logo from "../../img/dangoon.png"
 import Signout from "../signout/Signout";
 import Signin from "../signin/Signin";
 import Signup from "../signup/Signup"
-
-
-
-
 
 function Nav({ isLogin }) {
   // console.log(isLogin)
@@ -28,6 +26,13 @@ function Nav({ isLogin }) {
     signupRef.current.signupOpen();
   }
 
+  const rememberId = useSelector(state => state.User.id);
+  const isRememberId = useSelector(state => state.login.isRememberId);
+  const dispatch = useDispatch();
+  const onLogin = useCallback(() => dispatch(login()), [dispatch])
+  const onLogout = useCallback(() => dispatch(logout()), [dispatch]);
+  const onRememberId = useCallback(() => dispatch(rememberid()), [dispatch])
+  const onForgotId = useCallback(() => dispatch(forgotid()), [dispatch])
 
 
   return (
@@ -42,7 +47,7 @@ function Nav({ isLogin }) {
         <div className="login_menu">
           <div className="logout_btn">
             <button onClick={logoutOpenModal} >로그아웃</button>
-            <Signout isLogin={isLogin} ref={logoutRef} />
+            <Signout onLogout={onLogout} ref={logoutRef} />
           </div>
 
           <div className="menu">
@@ -53,7 +58,7 @@ function Nav({ isLogin }) {
           <div className="login_menu">
             <div className="logout_btn">
               <button onClick={loginOpenModal} >로그인</button>
-              <Signin ref={loginRef} />
+              <Signin rememberId={rememberId} onLogin={onLogin} isRememberId={isRememberId} onRememberId={onRememberId} onForgotId={onForgotId} ref={loginRef} />
             </div>
 
             <div className="signupBtnContainerWrap">
