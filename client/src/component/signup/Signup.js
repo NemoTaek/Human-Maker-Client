7250f851e67ffd9069b1f147d966324f58d18d19
NemoTaek <vkfkdchfhr@gmail.com>
2020-12-17 15:06:39 +0900

    doing: go: goal 기능 추가 및 css 수정

--- client/src/component/signup/Signup.js
+++ client/src/component/signup/Signup.js
@@ -72,7 +72,7 @@ const Signup = forwardRef((props, ref) => {
 		e.preventDefault();
 		if (idAvailable) {
 			axios
-				.post("https://humanmaker.ml/signup/idDoubleCheck", { id: id })
+				.post("http://localhost:5000/signup/idDoubleCheck", { id: id })
 				.then(res => {
 					if (res.status === 200) {
 						console.log(res);
@@ -83,7 +83,6 @@ const Signup = forwardRef((props, ref) => {
 						console.log(res);
 						setIdCheckMsg("사용 가능한 아이디 입니다.");
 						setIdCheck(true);
-						console.log(idCheck);
 					}
 				}).catch(err => {
 					console.log(err);
@@ -152,7 +151,7 @@ const Signup = forwardRef((props, ref) => {
 		e.preventDefault();
 		if (idCheck && pwCheck && pwDoubleCheck) {
 			axios
-				.post("https://humanmaker.ml/signup", userData)
+				.post("http://localhost:5000/signup", userData)
 				.then((res) => {
 					if (res.status === 201) {
 						alert("가입 되었습니다. 로그인 후 사용 가능합니다.");
